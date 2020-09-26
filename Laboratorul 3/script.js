6614da6c498d9cb01c0d3f6f3b2e41889d78ba64
adelina2999 <tataru.adelina99@gmail.com>
2020-09-26 18:00:21 +0300

    Laborator 3 TW

--- Laboratorul 3/script.js
+++ Laboratorul 3/script.js
@@ -1,4 +1,5 @@
-const correctAnswers = ["answer1d", "answer2b", "answer3e", "answer4c", "answer5e", "answer6b", "answer7c", "answer8d", "answer9e", "answer10d"]
+const correctAnswers = ["answer1a", "answer2a", "answer3a", "answer4a", "answer5a", "answer6a", "answer7a", "answer8a", "answer9a", "answer10a"]
+const spaceForTextIds = ["text-answer1", "text-answer2", "text-answer3", "text-answer4", "text-answer5", "text-answer6", "text-answer7", "text-answer8", "text-answer9", "text-answer10"]
 
 const answerIds = [
     ["answer1a", "answer1b", "answer1c", "answer1d", "answer1e"],
@@ -13,6 +14,45 @@ const answerIds = [
     ["answer10a", "answer10b", "answer10c", "answer10d", "answer10e"]
 ]
 
+let arrayWithCheckedItems = []
+let raspunsuriCorecte = 0
+
 function showResults(){
-    
+    for(let i = 0; i < answerIds.length; i++) {
+        for(let j = 0; j < answerIds[i].length; j++) {
+            if(document.getElementById(answerIds[i][j]).checked == true) {
+                arrayWithCheckedItems.push(answerIds[i][j])
+            }
+        }
+        document.getElementById(spaceForTextIds[i]).innerHTML = document.getElementById(arrayWithCheckedItems[i]).value
+        document.getElementById(spaceForTextIds[i]).style.fontWeight = "Bold"
+    }
+
+    colorSpaces()
+    document.getElementById("grade").innerHTML = raspunsuriCorecte
+}
+
+function colorSpaces() {
+    for(let i = 0; i < arrayWithCheckedItems.length; i++) {
+        if(arrayWithCheckedItems[i] == correctAnswers[i]) {
+            document.getElementById(spaceForTextIds[i]).style.color = "green"
+            raspunsuriCorecte++
+        } else {
+            document.getElementById(spaceForTextIds[i]).style.color = "red"
+        }
+    }
+}
+
+
+function stergere() {
+
+    for(let i = 0; i < answerIds.length; i++) {
+        for(let j = 0; j < answerIds[i].length; j++) {
+            document.getElementById(answerIds[i][j]).checked = false
+        }
+        document.getElementById(spaceForTextIds[i]).innerHTML = "_______________"
+        document.getElementById(spaceForTextIds[i]).style.color = "black"
+    }
+
+    arrayWithCheckedItems = []
 }
