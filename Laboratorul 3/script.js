7ad1bfb40c206d79f95237a47458b2cb350e878d
adelina2999 <tataru.adelina99@gmail.com>
2020-09-26 10:37:56 +0300

    Laboratorul 3

---
+++ Laboratorul 3/script.js
@@ -0,0 +1,18 @@
+const correctAnswers = ["answer1d", "answer2b", "answer3e", "answer4c", "answer5e", "answer6b", "answer7c", "answer8d", "answer9e", "answer10d"]
+
+const answerIds = [
+    ["answer1a", "answer1b", "answer1c", "answer1d", "answer1e"],
+    ["answer2a", "answer2b", "answer2c", "answer2d", "answer2e"],
+    ["answer3a", "answer3b", "answer3c", "answer3d", "answer3e"],
+    ["answer4a", "answer4b", "answer4c", "answer4d", "answer4e"],
+    ["answer5a", "answer5b", "answer5c", "answer5d", "answer5e"],
+    ["answer6a", "answer6b", "answer6c", "answer6d", "answer6e"],
+    ["answer7a", "answer7b", "answer7c", "answer7d", "answer7e"],
+    ["answer8a", "answer8b", "answer8c", "answer8d", "answer8e"],
+    ["answer9a", "answer9b", "answer9c", "answer9d", "answer9e"],
+    ["answer10a", "answer10b", "answer10c", "answer10d", "answer10e"]
+]
+
+function showResults(){
+    
+}
